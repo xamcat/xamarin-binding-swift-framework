d6eebadbc5eef3703f9c2b910e64d48e754d858d
Alexey Strakh <alexey.strakh@microsoft.com>
2019-10-04 14:25:01 -0700

    swift singleview demo project

---
+++ Swift/Swift.SingleView/Swift.SingleView/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Swift.SingleView
+//
+//  Created by Alexey Strakh on 10/4/19.
+//  Copyright © 2019 Xamarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//       btnLogin.TouchUpInside += btnLogin_Tap;
+//                 _proxy = new SwiftFrameworkProxy();
+//                 var result = _proxy.InitFor("APIKey");
+//                 System.Diagnostics.Debug.WriteLine(result);
+//                 lblMessage.Text = result;  }
+
+      }
+
+//      private void btnLogin_Tap(object sender, EventArgs e)
+//      {
+//          _proxy.Login(this);
+//      }
+}
+
