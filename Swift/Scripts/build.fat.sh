16e88c65de0061cd121b045725e2c2762cc1a7c0
Alexey Strakh <alexey.strakh@microsoft.com>
2019-10-18 20:20:16 -0700

    update script to generate fat lib and xamarin metadata

--- Swift/Scripts/build.fat.sh
+++ Swift/Scripts/build.fat.sh
@@ -6,8 +6,10 @@ SWIFT_PROJECT_NAME="SwiftFrameworkProxy"
 SWIFT_PROJECT_PATH="Swift/$SWIFT_PROJECT_NAME/$SWIFT_PROJECT_NAME.xcodeproj"
 SWIFT_BUILD_PATH="Swift/$SWIFT_PROJECT_NAME/build"
 SWIFT_OUTPUT_PATH="Swift/VendorFrameworks/swift-framework-proxy"
+XAMARIN_BINDING_PATH="Xamarin/SwiftFrameworkProxy.Binding"
 
 echo "Build iOS framework for simulator and device"
+rm -Rf "$SWIFT_BUILD_PATH"
 xcodebuild -sdk iphonesimulator$IOS_SDK_VERSION -project "$SWIFT_PROJECT_PATH" -configuration Release
 xcodebuild -sdk iphoneos$IOS_SDK_VERSION -project "$SWIFT_PROJECT_PATH" -configuration Release
 
@@ -22,7 +24,14 @@ echo "Verify results"
 lipo -info "$SWIFT_BUILD_PATH/Release-fat/$SWIFT_PROJECT_NAME.framework/$SWIFT_PROJECT_NAME"
 
 echo "Copy fat frameworks to the output folder"
+rm -Rf "$SWIFT_OUTPUT_PATH"
 mkdir "$SWIFT_OUTPUT_PATH"
-cp -R "$SWIFT_BUILD_PATH/Release-fat/$SWIFT_PROJECT_NAME.framework" "$SWIFT_OUTPUT_PATH"
+cp -Rf "$SWIFT_BUILD_PATH/Release-fat/$SWIFT_PROJECT_NAME.framework" "$SWIFT_OUTPUT_PATH"
+
+echo "Generating binding api definition and structs"
+sharpie bind --sdk=iphoneos$IOS_SDK_VERSION --output="$SWIFT_OUTPUT_PATH/XamarinApiDef" --namespace="Binding" --scope="$SWIFT_OUTPUT_PATH/$SWIFT_PROJECT_NAME.framework/Headers/" "$SWIFT_OUTPUT_PATH/$SWIFT_PROJECT_NAME.framework/Headers/$SWIFT_PROJECT_NAME-Swift.h"
+
+echo "Replace existing metadata with the udpated"
+cp -Rf "$SWIFT_OUTPUT_PATH/XamarinApiDef/*" "$XAMARIN_BINDING_PATH/"
 
 echo "Done!"
